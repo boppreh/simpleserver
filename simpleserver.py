f111dda1e424334167d687a802c4c2bb94f250d3
BoppreH <boppreh@gmail.com>
2013-09-30 00:47:52 -0300

    Move docs

--- simpleserver.py
+++ simpleserver.py
@@ -1,17 +1,16 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 class _HttpHandler(BaseHTTPRequestHandler):
-    """
-    Extracts the response from the class attribute "_HttpHandler.data".,
-    according to the path requested. GET /a/b/c is evaluated as
-    _HttpHandler.data['a']['b']['c']. Functions are replaced by their
-    returned values.
-    """
     def do_GET(self):
         result = navigate_value(_HttpHandler.data, self.path)
         self.wfile.write(str(result))
 
 def navigate_value(value, http_path):
+    """
+    Takes an initial value and a http path of the form "/some/path" and returns
+    `value['some']['path']`, converting list accesses to integer indexes and
+    replacing function values with their returns.
+    """
     for part in filter(bool, http_path.split('/')):
         # Use parts as list indexes.
         if isinstance(value, list):
